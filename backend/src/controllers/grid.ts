77f507326d7d7368e2ff23cb3b1a23ca6c67e446
Miguel Silva <miguel.castro.silva@clix.pt>
2023-10-23 17:23:51 +0100

    Run the validation of the bias parameter inside the controller

--- backend/src/controllers/grid.ts
+++ backend/src/controllers/grid.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express';
-import { matchedData, validationResult } from 'express-validator';
+import { matchedData, query, validationResult } from 'express-validator';
 
 import { generateRandomGrid, generateRandomGridWithBias, generateCode } from '../utils/grid';
 import { GRID_SIZE } from '../utils/constants';
 
 import { GridType } from '../types/grid';
 
-export const getGrid = (req: Request, res: Response) => {
-    const result = validationResult(req);
+export const getGrid = async (req: Request, res: Response) => {
+    await query('bias')
+        .isLength({ min: 1, max: 1 })
+        .withMessage('The bias parameter must contain one character.')
+        .isString()
+        .withMessage('The bias parameter must be a string.')
+        .isAlpha()
+        .withMessage('The bias parameter must be an alphabet character.')
+        .optional()
+        .run(req);
 
-    if (!result.isEmpty()) {
-        return res.status(400).send({ errors: result.array() });
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() });
     }
 
     const { bias } = matchedData(req);
--- backend/src/routes/grid.ts
+++ backend/src/routes/grid.ts
@@ -1,21 +1,9 @@
 import { Router } from 'express';
-import { query } from 'express-validator';
 
 import { getGrid } from '../controllers/grid';
 
 const router = Router();
 
-router.get(
-    '/',
-    query('bias')
-        .isLength({ min: 1, max: 1 })
-        .withMessage('The bias parameter must contain one character.')
-        .isString()
-        .withMessage('The bias parameter must be a string.')
-        .isAlpha()
-        .withMessage('The bias parameter must be an alphabet character.')
-        .optional(),
-    getGrid,
-);
+router.get('/', getGrid);
 
 export default router;
